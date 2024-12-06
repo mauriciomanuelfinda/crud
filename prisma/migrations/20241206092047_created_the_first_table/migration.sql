bf6db9eb556f7b956828d0c5b12a873e4a34910e
MauricioManuelFinda <mauriciomanuelfinda@gmail.com>
2024-12-06 12:27:57 +0100

    change

--- prisma/migrations/20241206092047_created_the_first_table/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Tarefas" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "completedAt" DATETIME,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updateAt" DATETIME NOT NULL
-);
