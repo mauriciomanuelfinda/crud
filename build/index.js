9d5e3ec7a6374ee849ba85728b8ddfbcfc1749f0
MauricioManuelFinda <mauriciomanuelfinda@gmail.com>
2024-12-06 13:51:03 +0100

    changed

---
+++ build/index.js
@@ -0,0 +1,31 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fastify_1 = __importDefault(require("fastify"));
+const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("@fastify/cors"));
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
+const app = (0, fastify_1.default)();
+const start = async () => {
+    try {
+        const port = Number(process.env.PORT);
+        await app.register(cors_1.default, {
+            origin: '*',
+            credentials: true
+        });
+        //Rotas
+        app.register(routes_1.default);
+        await app.listen({ port }).then(() => {
+            console.log(`Server is running on ${port}`);
+        });
+    }
+    catch (err) {
+        console.log(err);
+        app.log.error(err);
+        process.exit(1);
+    }
+};
+start();
